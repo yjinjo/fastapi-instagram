c2d8a502c1b8533a669f3ea8a0b6f63cdfbd9962
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-01 19:48:21 +0900

    #6. Adding a Second Router

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from router import blog_get
+from router import blog_post
 
 app = FastAPI()
 app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
 
 @app.get('/')
