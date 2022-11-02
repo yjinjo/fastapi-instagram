f56b58e80c1c795303d5626a813973b44934bd58
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-02 13:56:29 +0900

    #8. Create Database and Table

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from router import blog_get
 from router import blog_post
+from db import models
+from db.database import engine
 
 app = FastAPI()
 app.include_router(blog_get.router)
@@ -10,3 +12,6 @@ app.include_router(blog_post.router)
 @app.get('/')
 def index():
     return {'message': 'Hello world!'}
+
+
+models.Base.metadata.create_all(engine)
